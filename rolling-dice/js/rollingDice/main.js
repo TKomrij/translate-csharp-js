3886c25e5c966bc90d5ced6b24d887ea14eb8f71
Tatiane Komrij <tatiane.b.komrij@gmail.com>
2021-03-01 10:33:23 -0600

    get double roll values

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,30 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!")
+console.log("---------------------------")
+
+const Roll = (min, max) => {
+    min = Math.ceil(1);
+    max = Math.floor(6);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for (let i = 0; i < 10; i++) {
+    let die1 = Roll()
+    let die2 = Roll()
+
+    let message = `${die1} + ${die2} = ${die1 + die2}`
+
+    if (die1 === die2) {
+        message += " Doubles!"
+    }
+
+    console.log(message)
+}
+
+
+
+
+
+
+
